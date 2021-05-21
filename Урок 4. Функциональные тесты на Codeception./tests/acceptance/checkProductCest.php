fdb577c5b427dadfc9724652abbf3f6ad9abb1ee
bugd0ne <bogdanshvets.dev@gmail.com>
2021-05-22 02:12:15 +0600

    added working acceptance test

--- Урок 4. Функциональные тесты на Codeception./tests/acceptance/checkProductCest.php
+++ Урок 4. Функциональные тесты на Codeception./tests/acceptance/checkProductCest.php
@@ -6,5 +6,12 @@ class checkProductCest
     public function checkProductBloose(AcceptanceTester $I)
     {
         $I->amOnPage('');
+        $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
+        $I->waitForElement('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
+        $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
+        $I->switchToIFrame('.fancybox-iframe');
+        $I->seeElement('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
+        codecept_debug($I->grabTextFrom('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1'));
+
     }
 }
