cd9fb852a33f273e45658ecb1f10ab861f671e7a
bugd0ne <bogdanshvets.dev@gmail.com>
2021-05-25 16:26:36 +0600

    fixed test according to comments v2

--- Урок 4. Стартуем в Codeception./tests/acceptance/CheckProductCest.php
+++ Урок 4. Стартуем в Codeception./tests/acceptance/CheckProductCest.php
@@ -12,6 +12,6 @@ class CheckProductCest
         $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
         $I->switchToIFrame('.fancybox-iframe');
         $I->seeElement('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
-        $I->waitForText('Blouse');
+        $I->waitForText("Blouse", 10, '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
     }
 }
