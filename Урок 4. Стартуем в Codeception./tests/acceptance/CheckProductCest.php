d9652798d9aa9b95af02faea8f35e41df170505f
bugd0ne <bogdanshvets.dev@gmail.com>
2021-05-25 16:13:09 +0600

    fixed according to comments

--- Урок 4. Стартуем в Codeception./tests/acceptance/CheckProductCest.php
+++ Урок 4. Стартуем в Codeception./tests/acceptance/CheckProductCest.php
@@ -8,13 +8,13 @@ class CheckProductCest
         $I->amOnPage('');
         $I->scrollTo('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
         $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
-        $I->waitForElement('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
+        $I->waitForElementVisible('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
         $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
         $I->switchToIFrame('.fancybox-iframe');
         $I->seeElement('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
-        $I->see('Blouse');
+        $I->waitForText('Blouse');
         
-        // a little pending for not closing iframe after finding element with "Bloose" product
+        // a little pending for not closing iframe after finding element with text - "Bloose"
         $I->wait(3);
 
     }
