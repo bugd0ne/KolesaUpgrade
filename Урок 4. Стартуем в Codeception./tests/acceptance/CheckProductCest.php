abaef81d5643b8ce5a1b8bf76f64202b4f02bce5
bugd0ne <bogdanshvets.dev@gmail.com>
2021-05-27 05:35:21 +0600

    fixed locator for quickView button

--- Урок 4. Стартуем в Codeception./tests/acceptance/CheckProductCest.php
+++ Урок 4. Стартуем в Codeception./tests/acceptance/CheckProductCest.php
@@ -11,8 +11,8 @@ class CheckProductCest
         $blouseCardXPath = "//ul[@id='homefeatured']//li[2]//img";
 
         // #homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view
-        $quickViewCSS = 'li:nth-child(2) a.quick-view';
-        $quickViewXPath = "//li[2]//a[@class='quick-view']";
+        $quickViewCSS = '#homefeatured li:nth-child(2) a.quick-view';
+        $quickViewXPath = "//ul[@id='homefeatured']//li[2]//a[@class='quick-view']";
 
         // iframe selector
         $iFrameCSS = ".fancybox-iframe";
@@ -20,7 +20,7 @@ class CheckProductCest
 
         // #product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1
         $blouseHeaderCSS = "#product h1";
-        $blouseHeaderXpath = "//body[@id='product']//h1";
+        $blouseHeaderXPath = "//body[@id='product']//h1";
 
         $I->amOnPage('');
         $I->scrollTo('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
