e16ea2f0bfe369fd2673a3f5f6973bf2a6828600
bugd0ne <bogdanshvets.dev@gmail.com>
2021-06-01 12:18:16 +0600

    bootstraped codeception project for homework 7; added test and page for hw 7.1

---
+++ Урок 7. Паттерн Dataprovider/tests/acceptance/TabsMenuCest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class with tests for tabs 
+ */
+class TabsMenuCest
+{
+    /**
+     * test checks opening category in tab menu
+     */
+    public function checkCategoryOpening(AcceptanceTester $I)
+    {
+        $I->amOnPage('');
+    }
+}
